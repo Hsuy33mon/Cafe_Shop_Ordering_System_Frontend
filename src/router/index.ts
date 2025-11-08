d66f5e76e6f48ba374d4e0f3fe85d3a2932b0240
Min Pyae Hein <minpyaehein536h@prestigein.com>
2025-11-08 17:36:57 +0700

    change template

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
-import AboutPage from '@/views/about.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import aboutPage from "@/views/AboutPage.vue"
 const routes = [
-  { path: '/about', name: 'Home', component: AboutPage }
+  { path: '/about', name: 'about', component: aboutPage }
 ]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
 
-export default routes
+export default router
