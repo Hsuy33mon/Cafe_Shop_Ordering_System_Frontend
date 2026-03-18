209518833c3dcc280e9ae1aaacf4b12967bc2047
Min Pyae Hein <minpyaehein536h@prestigein.com>
2026-03-18 08:20:02 +0700

    fix the error

--- src/views/admin/AdminInvoiceDetails.vue
+++ src/views/admin/AdminInvoiceDetails.vue
@@ -169,20 +169,10 @@
         </table>
       </div>
     </section>
-
-    <!-- FOOTER -->
-    <section class="footer-actions">
-      <div>
-        <button class="btn-secondary">Edit</button>
-        <button class="btn-danger">Cancel Invoice</button>
-        <button @click="onPrint">Print Invoice</button>
-      </div>
-    </section>
   </main>
 </template>
 
 <script setup lang="ts">
-import { printInvoiceThermal } from '@/stores/thermalPrint'
 import { onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
@@ -245,10 +235,6 @@ async function updatePaymentStatus(paymentId: number) {
   } finally {
     saving[paymentId] = false
   }
-}
-async function onPrint() {
-  if (!invoice.value) return
-  await printInvoiceThermal(invoice.value)
 }
 </script>
 
